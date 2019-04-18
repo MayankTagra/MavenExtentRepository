4d1b27a70426c0ce248505436a663c53eed55d5e
MayankTagra <mayanktagra1@gmail.com>
2019-04-18 16:25:12 +0530

    Commit

--- Extent-app/src/test/java/SupportAndServices/InputSupport.java
+++ Extent-app/src/test/java/SupportAndServices/InputSupport.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
